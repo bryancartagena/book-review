d85530a86aa0dc435aedb68766d81a68eb768ebf
Bryan Cartagena <bryan.j.cartagena.b@gmail.com>
2025-09-21 13:54:15 -0600

    Add code for adding, updating and removing reviews by isbn

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -43,8 +43,48 @@ regd_users.post("/login", (req, res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const newReview = req.query.review;
+  const isbn = req.params.isbn;
+  const username = req.session?.authorization?.username;
+  if (!username || !isbn) {
+    return res.status(400).json({ message: "Unable to add review. Missing isbn or not authenticated." });
+  }
+  if (!newReview) {
+    return res.status(400).json({ message: "Please provide a review." });
+  }
+  const book = books[isbn];
+  if (!book) {
+    return res.status(400).json({ message: `Couldn't find book with isbn ${isbn}.` });
+  }
+  if (!book.reviews || typeof book.reviews !== 'object') {
+    book.reviews = {};
+  }
+  const exists = Object.prototype.hasOwnProperty.call(book.reviews, username);
+  book.reviews[username] = newReview;
+  return res.json({ message: exists ? 'Book review updated.' : 'Book review added.', reviews: book.reviews[username] });
+});
+
+// Remove a book review by user
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const username = req.session?.authorization?.username;
+  if (!username || !isbn) {
+    return res.status(400).json({ message: "Unable to add review. Missing isbn or not authenticated." });
+  }
+  const book = books[isbn];
+  if (!book) {
+    return res.status(400).json({ message: `Couldn't find book with isbn ${isbn}.` });
+  }
+  if (!book.reviews || typeof book.reviews !== 'object') {
+    book.reviews = {};
+  }
+  const exists = Object.prototype.hasOwnProperty.call(book.reviews, username);
+  if (!exists) {
+    return res.status(400).json({ message: "There's no review to delete." });
+  }
+
+  delete book.reviews[username];
+  return res.json({ message: "Review successfully removed." });
 });
 
 module.exports.authenticated = regd_users;
