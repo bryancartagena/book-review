57361f0f67a51ebc2c7d174499b12beeb4a7df8b
Bryan Cartagena <bryan.j.cartagena.b@gmail.com>
2025-09-21 11:28:13 -0600

    Add code to retrieve books by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -46,8 +46,20 @@ public_users.get('/author/:author', function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  let title = req.params.title;
+  const allBooks = Object.values(books);
+
+  if (!title) {
+    return res.status(400).json({ message: "Please provide a title" });
+  }
+
+  let book_filtered = allBooks.filter((book) => book.title.toLowerCase() === title.toLowerCase());
+
+  if (book_filtered.length === 0) {
+    return res.status(404).json({ message: `No books found by the title ${title}` });
+  }
+
+  return res.send(book_filtered);
 });
 
 //  Get book review
